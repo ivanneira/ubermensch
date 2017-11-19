5e8c715214c62c484f5b07326108952371fa7dff
Agustín Roth <agusrothdev@gmail.com>
2017-11-19 19:26:04 -0300

    Mejorado el RegEx. Ahora se envían tantos mensajes como ! hay en el String.

--- ubermensch_random.js
+++ ubermensch_random.js
@@ -1,24 +1,13 @@
-exports.randomMessage = function(api,message){
-
+exports.randomMessage = function(api, message) {
 	var reenviodemensajes = require('./ubermensch_reenviodemensajes.js')
 
-	//en caso de una llamada simple !random
-	if(/^![Rr][Aa][Nn][Dd][Oo][Mm]/.test(message.text)){
+	var patt = "/(!*)random/i"
 
-        var randomNumber = Math.floor((Math.random()* message.message_id ));
-
-        reenviodemensajes.reenvioAChat(api,message,randomNumber)
-    }
-
-    //en caso de una llamada doble !!random
-    if(/^!![Rr][Aa][Nn][Dd][Oo][Mm]/.test(message.text)){
-
-        var randomNumber = Math.floor((Math.random()* message.message_id ));
-
-        reenviodemensajes.reenvioAChat(api,message,randomNumber)
-
-        randomNumber = Math.floor((Math.random()* message.message_id ));
-
-        reenviodemensajes.reenvioAChat(api,message,randomNumber)
-    }
+	// Enviamos tantos mensajes como ! hay al principio.
+	if (patt.test(message.text)) {
+		for (i = 0; i < message.text.match(patt)[1].length); i++) {
+			var randomNumber = Math.floor((Math.random * message.message_id));
+			reenviodemensajes.reenvioAChat(api, message, randomNumber)
+		}
+	}
 }
