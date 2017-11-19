611179333fddf42ce435b4fbab453c9cf6a25a88
Agustín Roth <agusrothdev@gmail.com>
2017-11-19 19:31:05 -0300

    Mejorado el formato

--- ubermensch_enviarmensaje.js
+++ ubermensch_enviarmensaje.js
@@ -2,7 +2,7 @@ exports.envioDeMensaje = function(api, chat_id, texto, message_id) {
 	api.sendMessage({
 		chat_id: chat_id,
 		text: texto,
-		parse_mode:"Markdown",
+		parse_mode: "Markdown",
 		reply_to_message_id: message_id
 	})
 }
